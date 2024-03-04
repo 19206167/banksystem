7b61d47a7d05791ec74b986dab37f409fb4b67b0
Liu Zijian <liuzijian_2021@163.com>
2024-03-04 15:01:47 +0800

    get user card NUmber

---
+++ src/test/java/com/nus/team4/EnvironmentTest.java
@@ -0,0 +1,16 @@
+package com.nus.team4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+// test github workflow auto test
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnvironmentTest {
+
+    @Test
+    public void run(){}
+
+}
